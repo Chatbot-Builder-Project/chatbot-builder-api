86c7a0721b584df6519f5199de6cff8a4ff08c00
Izzat <work@izzatalsharif.com>
2024-11-09 01:17:25 +0200

    CB-6 Add API Versioning

--- ChatbotBuilderApi/DependencyInjection/PresentationServicesExtension.cs
+++ ChatbotBuilderApi/DependencyInjection/PresentationServicesExtension.cs
@@ -1,4 +1,6 @@
-﻿namespace ChatbotBuilderApi.DependencyInjection;
+﻿using Asp.Versioning;
+
+namespace ChatbotBuilderApi.DependencyInjection;
 
 public static class PresentationServicesExtension
 {
@@ -9,5 +11,19 @@ public static class PresentationServicesExtension
         services
             .AddControllers()
             .AddApplicationPart(Presentation.AssemblyReference.Assembly);
+
+        services.AddApiVersioning(options =>
+            {
+                options.ReportApiVersions = true;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            })
+            .AddMvc()
+            .AddApiExplorer(options =>
+            {
+                options.GroupNameFormat = "'v'VVV";
+                options.SubstituteApiVersionInUrl = true;
+            });
     }
 }
