76146ac553c4a4ce10e1eceb1f648a863bd4b95e
Izzat <work@izzatalsharif.com>
2024-11-09 01:11:32 +0200

    CB-6 Configure More Application Dependencies

--- ChatbotBuilderApi/Program.cs
+++ ChatbotBuilderApi/Program.cs
@@ -3,6 +3,7 @@ using ChatbotBuilderApi.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPresentationServices();
+builder.Services.AddApplicationServices();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
