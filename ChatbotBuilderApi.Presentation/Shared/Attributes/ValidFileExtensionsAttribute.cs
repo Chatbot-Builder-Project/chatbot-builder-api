c8555dc44b0395b5ce7a7ce76a743b4b486e81e7
Izzat <work@izzatalsharif.com>
2024-11-09 01:35:39 +0200

    CB-6 Add Presentation Attributes

---
+++ ChatbotBuilderApi.Presentation/Shared/Attributes/ValidFileExtensionsAttribute.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace ChatbotBuilderApi.Presentation.Shared.Attributes;
+
+public class ValidFileExtensionsAttribute : ValidationAttribute
+{
+    private readonly string[] _allowedExtensions;
+
+    public ValidFileExtensionsAttribute(string[] allowedExtensions)
+    {
+        _allowedExtensions = allowedExtensions;
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        switch (value)
+        {
+            case IFormFile file when !IsValidFileExtension(file.FileName):
+                return new ValidationResult(GetErrorMessage());
+            case IEnumerable<IFormFile> files:
+            {
+                if (files.Any(f => !IsValidFileExtension(f.FileName)))
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+
+                break;
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+
+    private bool IsValidFileExtension(string fileName)
+    {
+        var extension = Path.GetExtension(fileName).ToLowerInvariant().TrimStart('.');
+        return _allowedExtensions.Contains(extension);
+    }
+
+    private string GetErrorMessage()
+    {
+        return $"Invalid file type. Allowed types are {string.Join(", ", _allowedExtensions)}.";
+    }
+}
---
+++ ChatbotBuilderApi.Presentation/Shared/Attributes/ValidImageExtensionsAttribute.cs
@@ -0,0 +1,17 @@
+﻿using ChatbotBuilderApi.Application.Constants;
+
+namespace ChatbotBuilderApi.Presentation.Shared.Attributes;
+
+public class ValidImageExtensionsAttribute : ValidFileExtensionsAttribute
+{
+    public ValidImageExtensionsAttribute() : base(GetAllowedExtensions())
+    {
+    }
+
+    private static string[] GetAllowedExtensions()
+    {
+        return Enum.GetNames(typeof(ApplicationRules.File.ImageExtensions))
+            .Select(x => x.ToLower())
+            .ToArray();
+    }
+}
