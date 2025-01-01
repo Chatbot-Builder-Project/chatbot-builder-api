2b8b8ba446827f278819f2f7a0999e4f046bc6f9
Izzat <work@izzatalsharif.com>
2025-01-01 14:53:36 +0200

    CB-27 Migrate Images

---
+++ ChatbotBuilderApi.Persistence/Configurations/Images/ImageConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using ChatbotBuilderApi.Domain.Images;
+using ChatbotBuilderApi.Domain.Users;
+using ChatbotBuilderApi.Persistence.Configurations.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChatbotBuilderApi.Persistence.Configurations.Images;
+
+internal sealed class ImageConfiguration : IEntityTypeConfiguration<Image>
+{
+    public void Configure(EntityTypeBuilder<Image> builder)
+    {
+        builder.ConfigureAggregateRoot();
+
+        builder.HasKey(i => i.Id);
+        builder.Property(i => i.Id).ApplyEntityIdConversion();
+
+        builder.Property(i => i.Url).IsRequired();
+        builder.Property(i => i.Name).IsRequired();
+        builder.Property(i => i.ContentType).IsRequired();
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(i => i.OwnerId)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(i => i.OwnerId)
+            .HasColumnName("OwnerId");
+
+        builder.OwnsOne(i => i.Meta, metaBuilder =>
+        {
+            metaBuilder.Property(m => m.IsProfilePicture)
+                .HasColumnName("IsProfilePicture")
+                .IsRequired();
+
+            // Add shadow property to use in index
+            builder.Property<bool>("IsProfilePicture")
+                .HasColumnName("IsProfilePicture");
+        });
+
+        builder.HasIndex("OwnerId", "IsProfilePicture");
+
+        builder.HasIndex(i => i.Url).IsUnique();
+    }
+}
---
+++ ChatbotBuilderApi.Persistence/Migrations/20250101125233_AddImages.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatbotBuilderApi.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IsProfilePicture = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Image_AspNetUsers_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_OwnerId_IsProfilePicture",
+                table: "Image",
+                columns: new[] { "OwnerId", "IsProfilePicture" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_Url",
+                table: "Image",
+                column: "Url",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+        }
+    }
+}
