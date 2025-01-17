3d1f1f2f09518401503a4cd20de9ea8328223b54
Izzat <work@izzatalsharif.com>
2025-01-18 01:01:18 +0200

    CB-37 Add ImageDataJsonConverter

---
+++ ChatbotBuilderApi.Persistence/Configurations/Converters/Json/ImageDataJsonConverter.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using ChatbotBuilderApi.Domain.Graphs.ValueObjects.Data;
+
+namespace ChatbotBuilderApi.Persistence.Configurations.Converters.Json;
+
+public sealed class ImageDataJsonConverter : JsonConverter<ImageData>
+{
+    public override ImageData Read(
+        ref Utf8JsonReader reader,
+        Type typeToConvert,
+        JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        string? url = null;
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+                break;
+
+            if (reader.TokenType == JsonTokenType.PropertyName)
+            {
+                var propertyName = reader.GetString();
+
+                reader.Read(); // Move to the value
+
+                if (propertyName == nameof(ImageData.Url))
+                {
+                    url = reader.GetString();
+                }
+            }
+        }
+
+        if (url is null)
+        {
+            throw new JsonException("Missing required property Url");
+        }
+
+        return ImageData.Create(url);
+    }
+
+    public override void Write(
+        Utf8JsonWriter writer,
+        ImageData value,
+        JsonSerializerOptions options)
+    {
+        writer.WriteStartObject();
+        writer.WriteString(nameof(ImageData.Url), value.Url);
+        writer.WriteEndObject();
+    }
+}
